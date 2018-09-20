8667080a0a6c44d45e727e3261403939c8f372ba
Ntstewar <ntstewar@umail.iu.edu>
2018-09-20 12:16:44 -0400

    Polished Prototype v2 (Update)
    Cleaned up Directory more

---
+++ Current Build/Polished prototype v2/Assets/Restart.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+    
+
+
+{
+    private void Start()
+    {
+        Time.timeScale = 0.0f;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Time.timeScale = 1.0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+            Time.timeScale = 1.0f;
+        }
+    }
