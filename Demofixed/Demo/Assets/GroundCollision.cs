ff917959d38ebbc9ebcc4093dc69cdb655bb9587
Stephanie Davidson <rpookie98@gmail.com>
2018-09-19 23:36:53 -0400

    Resizing
    Changed Sprite Sizes

---
+++ Demofixed/Demo/Assets/GroundCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        // You probably want a check here to make sure you're hitting a zombie
+        // Note that this is not the best method for doing so.
+        if (collision.gameObject.name == "Ball")
+        {
+            print("hi");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
---
+++ Demofixed/Demo/Assets/Restart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Time.timeScale = 1.0f;
+        }
+    }
+}
